084b4087d70f27d66cad8df374fd0a265adb0e54
高庆丰 <feng@hemayun.com>
2017-10-19 23:42:07 +0800

    no message

--- owstoni.go
+++ owstoni.go
@@ -114,7 +114,6 @@ func (o *owstoni) Index(_ context.Context, m *MetadataInfo) (*Void, error) {
 	if err := m.save(); err != nil {
 		return nil, err
 	}
-	info("to.Index", m)
 	_indexChan <- m
 	return &Void{}, nil
 }
